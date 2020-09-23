3d78cd27fffcc9eebde199c8ac324f4b4456e2ff
rownok35 <50087009+rownok35@users.noreply.github.com>
2020-09-24 01:24:59 +0600

    Add files via upload

---
+++ chatbot_A4S.py
@@ -0,0 +1,74 @@
+import streamlit as st
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import numpy
+import tflearn
+import tensorflow
+import random
+import json
+import pickle
+
+st.title("Welcome to AI Based Chatbot")
+
+inp="hello"
+with open("intents.json") as file:
+	data = json.load(file)
+
+with open("data.pickle", "rb") as f:
+		words, labels, training, output = pickle.load(f)
+
+tensorflow.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+
+model.load("model.tflearn")
+
+
+def bag_of_words(s, words):
+	bag = [0 for _ in range(len(words))]
+
+	s_words = nltk.word_tokenize(s)
+	s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+	for se in s_words:
+		for i, w in enumerate(words):
+			if w == se:
+				bag[i] = 1
+			
+	return numpy.array(bag)
+
+
+	
+	
+
+string = st.text_area("chat here")
+if st.button("submit"):
+	inp=string
+
+
+# if inp.lower() == "quit":
+# 	break
+# st.write(type(inp))
+results = model.predict([bag_of_words(inp, words)])
+results_index = numpy.argmax(results)
+tag = labels[results_index]
+
+for tg in data["intents"]:
+	if tg['tag'] == tag:
+		responses = tg['responses']
+
+# print(random.choice(responses))
+st.write("Bot: ")
+result = random.choice(responses)
+st.success(result)
+
+
